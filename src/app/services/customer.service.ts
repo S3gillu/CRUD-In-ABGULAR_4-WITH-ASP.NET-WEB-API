965a7ef48746f3a290d27dd7693fa3821dfcf17f
S3gillu <shubhamsaurabh440@yahoo.com>
2017-11-16 15:20:33 +0530

    first commit

---
+++ src/app/services/customer.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, RequestOptions, Request, RequestMethod,Headers } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+
+@Injectable()
+export class CustomerService {
+  baseUrl: string = 'http://localhost:53811/api/Customer/GetAllCustomer';
+  baseUrl1: string = 'http://localhost:53811/api/Customer/AddCustomer';
+  baseUrl2: string = 'http://localhost:53811/api/Customer/DeleteCustomerById/';
+  baseUrl3: string = 'http://localhost:53811/api/Customer/GetCustomerById/';
+  
+
+  constructor(private _http: Http) { }
+
+  getCustomers() {
+
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    headers.append('Content-Type', 'application/json');
+    headers.append('Accept', 'application/json');
+    headers.append('Access-Control-Allow-Origin', 'http://localhost:6590');
+    let options = new RequestOptions({ headers: headers });
+
+
+    return this._http.get(this.baseUrl, options)
+      .map((response: Response) => response.json())
+      .catch(this._errorHandler);
+  }
+
+  getCustomerById(id) {
+
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+
+
+    return this._http.get(this.baseUrl3 + id)
+      .map((response: Response) => response.json())
+      .catch(this._errorHandler)
+  }
+
+  saveCustomer(customer) {
+
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+
+    return this._http.post(this.baseUrl1, customer)
+      .map((response: Response) => response.json())
+      .catch(this._errorHandler)
+  }
+
+  deleteCustomer(id) {
+  debugger;
+  
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+
+    return this._http.delete(this.baseUrl2 + id)
+      .map((response: Response) => response.json())
+      .catch(this._errorHandler)
+     
+  }
+
+  _errorHandler(error: Response) {
+    debugger;
+    console.log(error);
+    return Observable.throw(error || "Internal server error");
+  }
+}
